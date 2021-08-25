2f908872e8d46535444e2beb5b121d3e7ce9dee2
shubhampatidar1802 <69662766+shubhampatidar1802@users.noreply.github.com>
2021-08-26 00:47:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,15 +19,28 @@ const app=express()
 
 app.use(express.json())
 
-app.get('/' ,(req , res) =>{
-    res.send('API is running...')
-}) 
 
 app.use('/api/products',productRoutes) 
 
 app.use('/api/users',userRoutes) 
 
 app.use('/api/orders',orderRoutes) 
+
+const __dirname = path.resolve()
+app.use('/upload',express.static(path.join(__dirname,'/uploads')))
+
+if(process.env.NODE_ENV === 'production' ){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+
+}else{
+    app.get('/' ,(req , res) =>{
+        res.send('API is running...')
+    }) 
+}
 
 app.use(notFound)
 
